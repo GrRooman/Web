bbc65bd4ecf58ba6377abf58decc97d9267ed617
GrRooman <grushevskij.r@mail.ru>
2018-08-26 18:44:38 +0300

    Не большие правки кода

--- www/exercise3/PriceFilter.php
+++ www/exercise3/PriceFilter.php
@@ -52,7 +52,7 @@ if (!empty($_POST["submit"])) {
 
     if ($_POST["price_end"]) $where = addWhere($where, "`price` <= " . htmlspecialchars($_POST["price_end"])) . "";
 
-    $sql = "SELECT goods.name FROM `goods` JOIN price on goods.price_id=price.id";
+    $sql = "SELECT  goods.name,price.type_price,price.price FROM `goods` JOIN price on goods.price_id=price.price_id";
     if ($where) $sql .= " WHERE $where";
 }
 
@@ -69,10 +69,10 @@ function getQuer($e_link, $e_sql)
     for ($j = 0; $j < $rows; ++$j) {
         $row = mysqli_fetch_row($result);
         foreach ($row as $value) {
-            echo $value . " ";
+            echo $value . "   "."  ";
 
         }
-        echo '<br />';
+        echo '</br>';
     }
 }
 
