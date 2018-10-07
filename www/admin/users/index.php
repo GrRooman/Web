436cad537e535e91fa39d89134036c8d6c8859e0
GrRooman <grushevskij.r@mail.ru>
2018-10-07 19:01:06 +0300

    Компонент просмотра списка пользователей и просмотра детальной информации по пользователю.

--- www/admin/users/index.php
+++ www/admin/users/index.php
@@ -2,14 +2,24 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Пользователи");
 ?><?$APPLICATION->IncludeComponent(
-	"bitrix:main.user.link",
-	"",
-	Array(
-		"CACHE_TIME" => "7200",
+	"bitrix:forum.user.list", 
+	"users", 
+	array(
+		"COMPONENT_TEMPLATE" => "users",
+		"SHOW_USER_STATUS" => "N",
+		"URL_TEMPLATES_MESSAGE_SEND" => "",
+		"URL_TEMPLATES_PM_EDIT" => "",
+		"URL_TEMPLATES_PROFILE_VIEW" => "profile_view.php?UID=#UID#",
+		"URL_TEMPLATES_USER_POST" => "",
 		"CACHE_TYPE" => "A",
-		"ID" => "",
-		"NAME_TEMPLATE" => "#LAST_NAME# #NAME# #SECOND_NAME#",
-		"SHOW_LOGIN" => "Y",
-		"USE_THUMBNAIL_LIST" => "Y"
-	)
+		"CACHE_TIME" => "0",
+		"USERS_PER_PAGE" => "20",
+		"SET_NAVIGATION" => "Y",
+		"DATE_FORMAT" => "d.m.Y",
+		"DATE_TIME_FORMAT" => "d.m.Y H:i:s",
+		"PAGE_NAVIGATION_TEMPLATE" => "",
+		"SET_TITLE" => "Y",
+		"SEO_USER" => "Y"
+	),
+	false
 );?><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
--- www/test.php
+++ www/test.php
@@ -2,7 +2,36 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("test");
 ?><?$APPLICATION->IncludeComponent(
-	"bitrix:news.list",
-	"",
-Array()
+	"bitrix:forum.user.profile.view", 
+	".default", 
+	array(
+		"COMPONENT_TEMPLATE" => ".default",
+		"UID" => $_REQUEST["2"],
+		"URL_TEMPLATES_READ" => "read.php?FID=#FID#&TID=#TID#",
+		"URL_TEMPLATES_MESSAGE" => "message.php?FID=#FID#&TID=#TID#&MID=#MID#",
+		"URL_TEMPLATES_PROFILE" => "profile.php?UID=#UID#",
+		"URL_TEMPLATES_USER_LIST" => "user_list.php",
+		"URL_TEMPLATES_PM_LIST" => "pm_list.php",
+		"URL_TEMPLATES_MESSAGE_SEND" => "message_send.php?TYPE=#TYPE#&UID=#UID#",
+		"URL_TEMPLATES_PM_EDIT" => "pm_edit.php",
+		"URL_TEMPLATES_SUBSCR_LIST" => "subscr_list.php",
+		"URL_TEMPLATES_USER_POST" => "user_post.php?UID=#UID#&mode=#mode#",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "0",
+		"FID_RANGE" => array(
+		),
+		"DATE_FORMAT" => "d.m.Y",
+		"DATE_TIME_FORMAT" => "d.m.Y H:i:s",
+		"SEND_MAIL" => "E",
+		"SEND_ICQ" => "A",
+		"USER_PROPERTY" => array(
+		),
+		"SET_NAVIGATION" => "Y",
+		"SET_TITLE" => "Y",
+		"USER_PROPERTY_NAME" => "",
+		"SHOW_RATING" => "",
+		"RATING_ID" => "",
+		"RATING_TYPE" => ""
+	),
+	false
 );?><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
